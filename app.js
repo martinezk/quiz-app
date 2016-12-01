b2df092f2405bf812311a4f353cdca67d78cac95
Kristina Martinez <misskristinamartinez@gmail.com>
2016-12-01 06:27:59 -0500

    added more js

--- app.js
+++ app.js
@@ -3,38 +3,20 @@
 var quiz = {
 	quizQuestions:[
 		{
-			firstQuestion: "question-1",
+			firstQuestion: "question",
 			firstAnswers: ["ans-1", "ans-2", "ans-3",  "ans-4"]
 		}
 	]
 };
-var quesTemplate = (
-	'<h2>' + '</h2>' +
-	'<ul>' +
-		'<li><input type="radio" name="ans-1" value="1"><label for="ans-1">' + '</label></li>' +
-		'<li><input type="radio" name="ans-2" value="2"><label for="ans-2">' + '</label></li>' +
-		'<li><input type="radio" name="ans-3" value="3"><label for="ans-3">' + '</label></li>' +
-		'<li><input type="radio" name="ans-4" value="4"><label for="ans-4">' + '</label></li>' +
-	'</ul>'
-);
 
 //--store answer total
 var quizTotal = 0;
 
 //state modification functions
 //--if an answer is chosen, add to quiz total
-function userChoice(quiz, quizQuestion){	
-	for (var i=firstAnswers[1]; i<5; i++){
-		if (i===1){
-			quizTotal+=1;
-		} else if (i===2){
-			quizTotal+=2;
-		} else if (i===3){
-			quizTotal+=3;
-		} else {
-			quizTotal+=4;
-		}
-	}
+function userChoice(selectedAns){	
+	quizTotal+=selectedAns;
+	console.log(quizTotal);
 }
 
 //--delete current question
@@ -50,8 +32,22 @@ function deleteQuestion(){
 //event listenters
 //--start button listener
 //--next button listener 
-function checkAns1(){
-	var value = $('.question-1 input:checked').val();
-	console.log(value);
-}
-$('.question-1 > button').click(checkAns1)
+
+$(document).ready(function() {
+	$('#begin').click(function(){
+		$('div').first().removeClass('hidden').addClass('show');
+		$('#next').removeClass('hidden');
+		$(this).remove('button');
+	});
+	$('#next').click(function(){
+		var value = $('.show input:checked').val();
+		if(value === undefined){
+			alert("You haven't chosen one!");
+		} else {
+			value = parseInt(value);
+			userChoice(value);
+			$('.show').next().removeClass('hidden').addClass('show');
+			$('.show').first('div').removeClass('show').addClass('hidden');		
+		}
+	});
+})
